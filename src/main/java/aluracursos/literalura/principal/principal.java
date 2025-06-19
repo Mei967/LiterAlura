7c432013ac3e292e99021404b8ef1585521c4772
Meiby Burgos <meibyburgosa@gmail.com>
2025-06-18 22:46:37 -0400

    Agrega dependncia de Jackson y crea clase Principal

---
+++ src/main/java/aluracursos/literalura/principal/principal.java
@@ -0,0 +1,16 @@
+package aluracursos.literalura.principal;
+
+import aluracursos.literalura.service.ConsumoAPI;
+
+public class principal {
+
+    public class Principal {
+        public static void main(String[] args) {
+            ConsumoAPI consumo = new ConsumoAPI();
+            String url = "https://gutendex.com/books";
+            String json = consumo.obtenerDatos(url);
+
+            System.out.println(json); // Verifica si trae bien los datos
+        }
+    }
+}
