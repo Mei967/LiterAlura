f9bbdfa43b19ba02ac01978dbe5854c15fb18655
Meiby Burgos <meibyburgosa@gmail.com>
2025-06-19 02:10:38 -0400

    agregando constructor vacio y getters y setters a clase Libro y terminado la clase Autor

--- src/main/java/aluracursos/literalura/model/Autor.java
+++ src/main/java/aluracursos/literalura/model/Autor.java
@@ -1,12 +1,58 @@
 package aluracursos.literalura.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "libros")
+@Table(name = "autores")
 public class Autor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @JsonProperty("name")
+    private String nombre;
 
+    @JsonProperty("birth_year")
+    private Integer fechaDeNacimiento;
+
+    @JsonProperty("death_year")
+    private Integer fechaDeMuerte;
+
+    // Constructor vacío (requerido por JPA)
+    public Autor() {}
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(Integer fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public Integer getFechaDeMuerte() {
+        return fechaDeMuerte;
+    }
+
+    public void setFechaDeMuerte(Integer fechaDeMuerte) {
+        this.fechaDeMuerte = fechaDeMuerte;
+    }
 }
--- src/main/java/aluracursos/literalura/model/Libro.java
+++ src/main/java/aluracursos/literalura/model/Libro.java
@@ -1,5 +1,6 @@
 package aluracursos.literalura.model;
 
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
@@ -35,6 +36,64 @@ public class Libro {
     @ElementCollection
     private List<String> estanterias;
 
-}
+   // Constructor vacío requerido por JPA y Jackson
+    public Libro() {}
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(List<Autor> autores) {
+        this.autores = autores;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<String> getIdiomas() {
+        return idiomas;
+    }
+
+    public void setIdiomas(List<String> idiomas) {
+        this.idiomas = idiomas;
+    }
+
+    public Integer getCantidadDescargas() {
+        return cantidadDescargas;
+    }
+
+    public void setCantidadDescargas(Integer cantidadDescargas) {
+        this.cantidadDescargas = cantidadDescargas;
+    }
+
+    public List<String> getEstanterias() {
+        return estanterias;
+    }
+
+    public void setEstanterias(List<String> estanterias) {
+        this.estanterias = estanterias;
+    }
+}
 
