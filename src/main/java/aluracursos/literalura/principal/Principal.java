53af5b8851f870246d5b0800834d37439ffeeb9d
Meiby Burgos <meibyburgosa@gmail.com>
2025-06-18 23:16:37 -0400

    Corrige nombre del archivo Principal.java

--- src/main/java/aluracursos/literalura/principal/Principal.java
+++ src/main/java/aluracursos/literalura/principal/Principal.java
@@ -2,15 +2,15 @@ package aluracursos.literalura.principal;
 
 import aluracursos.literalura.service.ConsumoAPI;
 
-public class principal {
 
     public class Principal {
         public static void main(String[] args) {
             ConsumoAPI consumo = new ConsumoAPI();
             String url = "https://gutendex.com/books";
             String json = consumo.obtenerDatos(url);
+            System.out.println("Respuesta JSON recibida:");
 
             System.out.println(json); // Verifica si trae bien los datos
         }
     }
-}
+
