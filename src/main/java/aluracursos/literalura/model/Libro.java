a6ab19ab5930ef811d632639ff87031b099772e1
Meiby Burgos <meibyburgosa@gmail.com>
2025-06-19 03:26:57 -0400

    modificando clase libro

--- src/main/java/aluracursos/literalura/model/Libro.java
+++ src/main/java/aluracursos/literalura/model/Libro.java
@@ -14,9 +14,30 @@ public class Libro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonProperty("title")
+    private String titulo;
+
+    @JsonProperty("authors")
+    @ElementCollection
+    private List<Autor> autores;
+
+    @JsonProperty("summaries")
+    @Column(length = 1000)
+    private String descripcion;
+
+    @JsonProperty("languages")
+    @ElementCollection
+    private List<String> idiomas;
+
+    @JsonProperty("download_count")
+    private Integer cantidadDescargas;
+
+    @JsonProperty("bookshelves")
+    @ElementCollection
+    private List<String> estanterias;
 
 
-   // Constructor vacío requerido por JPA y Jackson
+    // Constructor vacío requerido por JPA y Jackson
     public Libro() {}
 
 
